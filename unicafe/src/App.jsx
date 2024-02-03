4e8571d4b4fc8f152073d6548ef017b3ed223170
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-02-03 12:28:16 +0200

    Finish exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -8,31 +8,48 @@ const Display = (props) => (
   <p>good: {props.good}</p>
   <p>neutral: {props.neutral}</p>
   <p>bad: {props.bad}</p>
+  <p>total: {props.total}</p>
+  <p>average: {props.average}</p>
+  <p>postitive : {props.positive}</p>
   </div>
 
 
 )
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState([])
+  const average = score.reduce((accumulator,currentValue) => accumulator + currentValue, 0)/score.length;
+  const positiveScore = score.filter(num => num > 0).length;
+  const positive = (positiveScore/score.length) * 100
 
 
-  const handler = (quality, setQuality) => {
+
+  const handler = (quality, setQuality, scoreValue) => {
     const newQuality = quality +1;
     setQuality(newQuality);
-    console.log(newQuality)
+    const newTotal = total +1;
+    setTotal(newTotal);
+    setScore(score.concat(scoreValue));
+    
   }
   
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handler={()=>handler(good, setGood)} text="good" />
-      <Button handler={()=>handler(neutral, setNeutral)} text="neutral" />
-      <Button handler={()=>handler(bad, setBad)} text="bad" />
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Button handler={()=>handler(good, setGood, 1)} text="good" />
+      <Button handler={()=>handler(neutral, setNeutral, 0)} text="neutral" />
+      <Button handler={()=>handler(bad, setBad, -1)} text="bad" />
+      <Display good={good} 
+               neutral={neutral}
+               bad={bad}
+               total={total} 
+              
+               average={average ? average : "No feedback received"} 
+               positive={positive ? (positive + " %" ) : "No feedback received"} />
     </div>
   )
 }
