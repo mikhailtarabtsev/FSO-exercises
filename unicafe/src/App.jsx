94902c4168f3a590da432f143b3ebbfb7ca338a8
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-02-03 13:05:20 +0200

    finish exercise 1.10

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 
 const Button = (props) => (<button onClick={props.handler}>{props.text}</button>)
-
 const Statistics = (props) => {
   if (props.score.length == 0) {
-
     return (
       <>
-      <h2>statistics</h2>
       <p>No feedback has been received</p>
       </>
     )
@@ -15,19 +12,23 @@ const Statistics = (props) => {
   else {
     return(
       <>
-  <h2>statistics</h2>
-  <p>good: {props.good}</p>
-  <p>neutral: {props.neutral}</p>
-  <p>bad: {props.bad}</p>
-  <p>total: {props.total}</p>
-  <p>average: {props.average}</p>
-  <p>postitive : {props.positive}</p>
-  </>
+      <StatisticLine text="good:" value={props.good}/>
+      <StatisticLine text="neutral:" value={props.neutral}/>
+      <StatisticLine text="bad:" value={props.bad}/>
+      <StatisticLine text="total:" value={props.total}/>
+      <StatisticLine text="average:" value={props.average}/>
+      <StatisticLine text="positive:" value={props.positive + " %"}/>
+      </>
 
 
     )
   }
-  
+}
+
+const StatisticLine =(props) =>{
+  return (
+    <p>{props.text} : {props.value}</p>
+  )
 }
 
 
@@ -59,14 +60,15 @@ const App = () => {
       <Button handler={()=>handler(good, setGood, 1)} text="good" />
       <Button handler={()=>handler(neutral, setNeutral, 0)} text="neutral" />
       <Button handler={()=>handler(bad, setBad, -1)} text="bad" />
+      <h2>statistics</h2>
       <Statistics good={good} 
                neutral={neutral}
                bad={bad}
                total={total}
                score = {score}
               
-               average={average ? average : "No feedback received"} 
-               positive={positive ? (positive + " %" ) : "No feedback received"} />
+               average={average} 
+               positive={positive} />
     </div>
   )
 }
