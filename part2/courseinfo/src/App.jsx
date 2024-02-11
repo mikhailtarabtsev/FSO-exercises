05df5a03b61f4406b7e3993467eecc2e74b320ea
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-02-11 19:57:54 +0200

    finish exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,4 @@
+import Course from "./course"
 const App = () => {
   const courses = [
     {
@@ -44,54 +45,7 @@ const App = () => {
     }
   ]
 
-  const Header =({name}) => {
-    return (
-      <h2>{name}</h2>
-    )
-  }
-
-
-  const Content = ({parts}) => {
-    return(
-      <>
-      {parts.map(part => 
-          <Part name = {part.name} exercises = {part.exercises} key = {part.id} />
-      )
-      }
-      </>
-    )
-  }
-
-
-  const Part = ({name, exercises}) => {
-    return (
-      <p>
-      {name} {exercises}
-      </p>
-    )
-  }
-
-  const Total = ({parts}) =>{
-    const exercises =  parts.map(part =>part.exercises )
-    const total = exercises.reduce((sum, value) => sum+value)
-
-    return (
-      <h3>Total of {total} exercises </h3>
-    )
-
-  }
-
-  const Course = ({course}) => {
-      return(
-      <>
-    
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </>
-    )
-
-  }
+ 
 
   return (
     <>
