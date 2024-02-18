df56c3ba32585acbe5657c2507f3b99a3712f6f5
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-02-18 19:47:19 +0200

    Replace emoji flag for svg with alt description

--- part2/countries/src/components/country.jsx
+++ part2/countries/src/components/country.jsx
@@ -3,15 +3,13 @@ const Country = ({result}) =>{
     return (
         <>
             <h2>{result.name.common}</h2>
-                <p>{result.capital[0]}</p>
-                <p>{result.area}</p>
+                <p>Capital: {result.capital[0]}</p>              
+                <p>Total area: {result.area}</p>
             <h3>Languages</h3>
                 <ul>
                 {Object.entries(result.languages).map(([key, value]) => (<li key = {key}>{value}</li>))}
                 </ul>
-            <p style={{
-                fontSize:"10em",
-                margin: 0}}>{result.flag}</p>
+            <img style={{width: 500}} src={result.flags.svg} alt={result.flags.alt} />
         </>
     )
 }
