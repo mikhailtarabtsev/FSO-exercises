8246989dc717cc8687b621495bf1ec02eaba2af5
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-02-19 21:39:09 +0200

    Finish exercise 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -30,6 +30,20 @@ app.get("/api/persons", (req, res)=>{
     res.json(data)
 })
 
+app.get("/api/persons/:id", (req,res)=> {
+    const id = Number(req.params.id)
+    
+    const person = data.find(contact => id === contact.id)
+
+    if (person){
+        res.json(person)
+    }
+    else {
+        res.status(404).end()
+    }
+    
+})
+
 app.get("/info", (req, res) =>{
     const date = new Date()
     res.send(`
