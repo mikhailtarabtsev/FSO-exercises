eea19538fec60ab99df8c3f2590ee80076598206
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-03-15 18:49:17 +0200

    Refactor schema into separate module

--- part4/Blog_list/index.js
+++ part4/Blog_list/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const {mongoUrl, port} = require('./utils/config')
+const mongoose = require('mongoose')
 
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+const Blog = require('./models/blog')
 
 mongoose.connect(mongoUrl)
 
