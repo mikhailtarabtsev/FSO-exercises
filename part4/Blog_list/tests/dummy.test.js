b00f6b086fe6200f3e83ee9b0fb9d67a4544f79c
Mikhail Tarabtsev <mikhail.tarabtsev@gmail.com>
2024-03-17 19:14:52 +0200

    Finish exercise 4.4

--- part4/Blog_list/tests/dummy.test.js
+++ part4/Blog_list/tests/dummy.test.js
@@ -1,10 +1,73 @@
-const {test} = require('node:test')
+const {test, describe} = require('node:test')
 const assert = require('node:assert')
-const dummy = require('../utils/list_helper').dummy
+const listHelper = require('../utils/list_helper')
 
+const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
 test('dummy test returns 1', ()=> {
-    const blogs = []
 
-    const result = dummy(blogs)
+    const result = listHelper.dummy(blogs)
     assert.strictEqual = (result, 1)
 })
+
+describe('total likes', ()=>{
+        test('with one blog', () => {
+            const oneBlog = [blogs[0]]
+            assert.strictEqual = (listHelper.totalLikes(oneBlog, 7))
+        })
+        test('empty is zero', ()=>{
+            const empty = []
+            assert.strictEqual = (listHelper.totalLikes(empty, 0))
+        })
+        test('full list is total', ()=>{
+            assert.strictEqual = (listHelper.totalLikes(blogs), 36)
+        })
+    })
--- part4/Blog_list/utils/list_helper.js
+++ part4/Blog_list/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, value) => ( sum + value.likes||0)
+    return  blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
